bcd19cf930a4b10f115527c5f4d73958ea31375c
Tom <wiredzenith@gmail.com>
2019-11-11 21:32:22 +0000

    first test for atmega 324PB

---
+++ Atmel_324PB_test/Atmel_324PB_test/src/main.c
@@ -0,0 +1,50 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * Bare minimum empty user application template
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# "Insert system clock initialization code here" comment
+ * -# Minimal main function that starts with a call to board_init()
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+#include <asf.h>
+
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	board_init();
+	
+	bool button_state;
+	while(1){
+		button_state = ioport_get_pin_level(GPIO_PUSH_BUTTON_0);
+		if(button_state){
+			LED_Off(LED0);
+			}else{
+			LED_On(LED0);
+		}
+	}
+
+	/* Insert application code here, after the board has been initialized. */
+}
