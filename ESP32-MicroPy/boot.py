cd6950f0de35ecbe07b27a56f3d3b2a5232626c0
pigeon_strike <wiredzenith@gmail.com>
2020-05-04 23:15:15 +0100

    pull contacts from database, message multiple users

--- ESP32-MicroPy/boot.py
+++ ESP32-MicroPy/boot.py
@@ -8,32 +8,50 @@ import gsmFunct
 from machine import Pin
 from machine import Timer
 
-gsmFunct.initGsm()
 
 
 def timerIrq_0(timer):
+    """[summary]
+
+    Arguments:
+        timer {[type]} -- [description]
+    """
     if (RED_LED.value() == True):
         RED_LED.value(0)
     else:
         RED_LED.value(1)
 
 
-def SwInterrupt(pin):
+#* end of timerIrq_0
+
+
+def swInterrupt(pin):
+    """[summary]
+
+    Arguments:
+        pin {[type]} -- [description]
+    """
     print('SW Pressed')
-    gsmFunct.sendSMS()
+    gsmFunct.sendSMS("00353852302163", "hey sexy")
     if (GRN_LED.value() == 1):
         GRN_LED.value(0)
     else:
-        GRN_LED.value(1) 
+        GRN_LED.value(1)
 
+
+#* end of swInterrupt
 
 RED_LED = Pin(13, Pin.INOUT)  # create output
 GRN_LED = Pin(12, Pin.INOUT)  # create output
 
-#create pin 22 and set it as input and enable pull up
-SW = Pin(14, Pin.IN,Pin.PULL_UP,trigger=Pin.IRQ_FALLING, handler=SwInterrupt,debounce=200) 
+# create pin 22 and set it as input and enable pull up
+SW = Pin(14,
+         Pin.IN,
+         Pin.PULL_UP,
+         trigger=Pin.IRQ_FALLING,
+         handler=swInterrupt,
+         debounce=200)
 
 timer0 = Timer(0)
 timer0.init(period=200, mode=Timer.PERIODIC, callback=timerIrq_0)
-
 
--- ESP32-MicroPy/gsmFunct.py
+++ ESP32-MicroPy/gsmFunct.py
@@ -1,16 +1,19 @@
-import machine, time, sys 
+import machine, time, sys
+import os
 import gsm
+import json
 import urequests as req
 
 #Variables
 
-usersList = []
+
+contactsPath = "contactFile.txt"
 
 # APN credentials
 
-GSM_APN  = 'data.myeirmobile.ie' # Your APN
-GSM_USER = '' # Your User
-GSM_PASS = '' # Your Pass
+GSM_APN = 'data.myeirmobile.ie'  # Your APN
+GSM_USER = ''  # Your User
+GSM_PASS = ''  # Your Pass
 
 # Power on the GSM module
 
@@ -24,83 +27,163 @@ GSM_MODEM_PWR.value(1)
 
 GRN_LED = machine.Pin(12, machine.Pin.INOUT)  # create output
 
-# Init PPPoS
-def initGsm():
-    gsm.debug(True)  # this to see more logs, investigate issues, etc.
+#* Init PPPoS
 
-    gsm.start(tx=27, rx=26, apn=GSM_APN, user=GSM_USER, password=GSM_PASS)
+gsm.debug(True)  # this to see more logs, investigate issues, etc.
 
-    sys.stdout.write('Waiting for AT command response...')
-    for retry in range(20):
-        if gsm.atcmd('AT'):
-            break
-        else:
-            sys.stdout.write('.')
-            time.sleep_ms(5000)
+gsm.start(tx=27, rx=26, apn=GSM_APN, user=GSM_USER, password=GSM_PASS)
+
+sys.stdout.write('Waiting for AT command response...')
+for retry in range(20):
+    if gsm.atcmd('AT'):
+        break
     else:
-        raise Exception("Modem not responding!")
-    print()
+        sys.stdout.write('.')
+        time.sleep_ms(5000)
+else:
+    raise Exception("Modem not responding!")
+print()
 
-def resB():
-    gsmFunct.gsm.atcmd('AT+CUSD=1')
-    gsmFunct.gsm.atcmd('AT+CUSD=1,"*#100#",15', printable=True)
-    time.sleep_ms(5000)
-            
-def smsCB(indexes):
+#* end of Init PPPoS
+
+
+
+#------------Functions-------------------#
+
+
+# def uart_cb(res): #! WIP
+#     """[summary]
+
+#     Arguments:
+#         res {[type]} -- [description]
+#     """
+
+#     print("[UART CB]", res)
+
+# #* end of uart_cb
+
+
+def sendSMS(outgoingNumber, outgoingMsg):
+    """[summary]
+
+    Arguments:
+        outgoingNumber {[type]} -- [description]
+        outgoingMsg {[type]} -- [description]
+    """
+    gsm.sendSMS(outgoingNumber, outgoingMsg)
+
+#* end of sendSMS gsmFunct.sendMessageToAllContacts()
+
+def sendMessageToAllContacts():
+    
+    with open(contactsPath, 'r') as contacts_json:
+        contactsList = json.load(contacts_json)
+
+    for contact in contactsList:
+        recipient = contact['name']
+        outgoingNumber = contact['number']
+        outgoingMsg = "Dear " + recipient + " we regret to inform you that an emergency alert has been triggered"
+        sendSMS(outgoingNumber, outgoingMsg)
+    
+
+def getContacts():
+    """[summary]
+
+    Returns:
+        [type] -- [description]
+    """
+    
+    usersList = []
+    gsm.connect()
+    if gsm.status() == "98, 'Not started'":
+        gsm.connect()
+    r = req.get("http://wiredzenith.tech/contacts/list")
+    usersList = r.json()
+    gsm.disconnect()
+
+    return usersList
+
+#* end of getContacts
+
+def saveContactsToFile():
+    usersList = []
+        
+    try:
+        os.remove(contactsPath)
+    except OSError:
+        print("File doesn't exist, Creating " + contactsPath)
+    finally:
+        usersList = getContacts()
+        
+    with open(contactsPath, 'w') as outfile:
+        json.dump(usersList, outfile)
+
+    with open(contactsPath) as json_file:
+        data = json.load(json_file)
+        print(data)
+
+#* end of saveContactsToFile
+
+def smscb(indexes):
+    """[summary]
+
+    Arguments:
+        indexes {[type]} -- [description]
+    """
     if indexes:
         print(indexes[0])
         msg = gsm.readSMS(int(indexes[0]), True)
         reqMsg = msg[6]
-        
-        def resA():
-            if (GRN_LED.value() == 1):
-                GRN_LED.value(0)
-            else:
-                GRN_LED.value(1)
-        
-        def resB():
-            GsmFunct.gsm.atcmd('AT+CUSD=1')
-            GsmFunct.gsm.atcmd('AT+CUSD=1,"*#100#",15', printable=True)
-            
-            
-            
-        
-                
-        def resInvalid():
-            sendersNumber = msg[2]
-            sendersMsg = msg[6]
-            reply = "The request you made ' " + sendersMsg + "' is not a valid request. Please try again."
-            sendSMS(sendersNumber, reply)
-              
-        
-        
-        def receivedMessage(reqMsg):
-            switch = {
-            
-            'Done':resA,
-            'Balance':resB
-            }
-            switch.get(reqMsg,lambda: resInvalid())
 
         if msg:
             print("New message from", msg[2])
-            print(msg[6])
-            
-def sendSMS(outgoingNumber , outgoingMsg):
-    gsm.sendSMS(outgoingNumber, outgoingMsg)
-    
-def getContacts():
-    gsm.connect()
-    if gsm.status() == "98, 'Not started'" :
-        pass
-        r = req.get("http://wiredzenith.tech/contacts/list")
-        usersList = r.json()
-        gsm.disconnect()
-        return usersList
-    
-    
+            inboundMsg = msg[6].strip().lower()
+            sendersNo = msg[2]
+            print(inboundMsg)
+            if inboundMsg == "test":
+                sendSMS(sendersNo, "test this sexy")
+
+            elif inboundMsg == "hello":
+                sendSMS(sendersNo, "hello this sexy")
+
+            elif inboundMsg == "balance":
+                sendSMS(sendersNo, "balance this sexy")
+
+            elif inboundMsg == "contacts":
+                sendSMS(sendersNo, "contacts sexy")
+
+            elif inboundMsg == "my number":
+                sendSMS(sendersNo, "0852302163")
+            elif inboundMsg == "999":
+                sendMessageToAllContacts()
+            elif inboundMsg is "led":
+                if (GRN_LED.value() == 1):
+                    GRN_LED.value(0)
+                else:
+                    GRN_LED.value(1)
+            else:
+                sendSMS(
+                    sendersNo,
+                    """\rHelp: test = a sext test balance = sexy balance contacts = sexy contacts my number = my number """
+                )
+
+
+#* end of smscb()
+
+# def resA():
+#     if (GRN_LED.value() == 1):
+#         GRN_LED.value(0)
+#     else:
+#         GRN_LED.value(1)
 
 
 
+  
+#! WARNING do not add this again, causes device to hang and require reflash
+#uart = machine.UART(2, tx=27, rx=26, timeout=5000,  buffer_size=1024)
+#uart.callback(uart.CBTYPE_PATTERN, uart_cb, pattern= "D: ")
 
-gsm.sms_cb(smsCB, 2)
+# Get contacts list when the device starts
+saveContactsToFile() 
+
+gsm.sms_cb(smscb, 5)
